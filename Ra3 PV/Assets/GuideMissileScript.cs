9a8225fd60ea7b71ff64a020a10844cfedb68068
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2020-05-30 17:50:37 +0800

    Adding Sea

---
+++ Ra3 PV/Assets/GuideMissileScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideMissileScript : MonoBehaviour
+{
+
+    public float Speed;
+    public Transform target;
+    public float RotationDelta;
+
+    [Space]
+    public float Accelerator;
+    [Space]
+    public GameObject HitFX;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        if (HitFX != null)
+        {
+            Instantiate(HitFX, transform.position, transform.rotation);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion TargetQuaternion = new Quaternion();
+        if (target)
+        {
+            GameObject TargetingHelper = new GameObject("TH");
+            TargetingHelper.transform.parent = transform;
+            TargetingHelper.transform.localPosition = new Vector3(0, 0, 0);
+            TargetingHelper.transform.LookAt(target);
+            TargetQuaternion = TargetingHelper.transform.rotation;
+            transform.rotation = Quaternion.Lerp(transform.rotation, TargetingHelper.transform.rotation,RotationDelta);
+            Destroy(TargetingHelper);
+            
+        }
+        transform.Translate(0, 0, Speed);
+
+        //Speed += Accelerator;
+        if (transform.rotation == TargetQuaternion)
+        {
+            Speed += Accelerator;
+        }
+
+    }
+}
---
+++ Ra3 PV/Assets/KirovController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KirovController : MonoBehaviour
+{
+    public Transform[] UpPropellers;
+    public Transform[] FrontPropellers;
+    public float RotateSpeed;
+    [Space]
+    public Transform[] RocketTransforms;
+    public Transform[] OriginalRocketTransforms;
+    public Transform[] LoadedRocketTransforms;
+    public float ReloadSpeed;
+    public float ReloadProcess;
+    public GameObject Rocket;
+    public GameObject LauchFX;
+    public int Number = 1;
+    public Transform Target;
+    [Space]
+    public float MovingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int I = 0; I < RocketTransforms.Length; I++)
+        {
+            RocketTransforms[I].position= LoadedRocketTransforms[I].position;
+            //print("A");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int Iu = 0; Iu < UpPropellers.Length; Iu++)
+        {
+            UpPropellers[Iu].Rotate(0, 0, RotateSpeed);
+            //print("A");
+        }
+        for (int If = 0; If < FrontPropellers.Length; If++)
+        {
+            FrontPropellers[If].Rotate(RotateSpeed, 0, 0);
+            //print("B");
+        }
+
+        if (ReloadProcess <= 0)
+        {
+            RocketTransforms[Number-1].position = OriginalRocketTransforms[Number-1].position;
+            Instantiate(LauchFX, LoadedRocketTransforms[Number - 1].position, LoadedRocketTransforms[Number - 1].rotation);
+            Instantiate(Rocket, LoadedRocketTransforms[Number-1].position, LoadedRocketTransforms[Number-1].rotation);
+            Rocket.GetComponent<GuideMissileScript>().target = Target;
+            ReloadProcess = ReloadSpeed;
+            Number += 1;
+            if (Number > RocketTransforms.Length)
+            {
+                Number = 1;
+            }
+        }
+
+        if (ReloadProcess > 0)
+        {
+            ReloadProcess -= 1;
+        }
+
+        for (int I = 0; I < RocketTransforms.Length; I++)
+        {
+            if(RocketTransforms[I].position!= LoadedRocketTransforms[I].position)
+            RocketTransforms[I].position = Vector3.Lerp(RocketTransforms[I].position, LoadedRocketTransforms[I].position, 0.05f);
+            //print("A");
+        }
+
+    }
+}
