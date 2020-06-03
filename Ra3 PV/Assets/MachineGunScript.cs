3010c9106a134e3af32587bc680460c4f7a46f2c
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2020-06-03 18:38:25 +0800

    optimizing

---
+++ Ra3 PV/Assets/MachineGunScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGunScript : MonoBehaviour
+{
+    public bool Trigger;
+    [Space]
+    public Transform BulletSpawnPoint;
+    public Transform RotateBarrel;
+    public Vector3 BarrelRotateSpeed;
+    [Space]
+    public float FireRate;
+    public float ReloadProcess;
+    public GameObject Shell;
+    //public Vector3 ShellForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Trigger)
+        {
+            if (RotateBarrel)
+            {
+                RotateBarrel.Rotate(BarrelRotateSpeed);
+            }
+
+            if (ReloadProcess <= 0)
+            {
+                Instantiate(Shell, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+                //Shell.GetComponent<Rigidbody>().AddRelativeForce(ShellForce);
+                ReloadProcess = FireRate;
+            }
+            if (ReloadProcess > 0)
+            {
+                ReloadProcess -= 1;
+            }
+        }
+    }
+}
---
+++ Ra3 PV/Assets/StartAddRelativeForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAddRelativeForce : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Vector3 Force;
+    public float Rotationf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 CF = Force += new Vector3(Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)), Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)), Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)));
+        rigidbody.AddRelativeForce(CF);
+        //rigidbody.AddRelativeTorque(Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)), Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)), Random.Range(Mathf.Abs(Rotationf), -Mathf.Abs(Rotationf)));
+        //GetComponent<StartAddRelativeForce>()
+        Destroy(GetComponent<StartAddRelativeForce>());
+    }
+
+    
+}
